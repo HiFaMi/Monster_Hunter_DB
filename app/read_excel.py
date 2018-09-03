09051d1d2df4a6f736a51925f7ff820c733f6167
mingyu <rlaalsrbgk@gmail.com>
2018-09-03 20:29:49 +0900

    hammer 업데이트

--- app/read_excel.py
+++ app/read_excel.py
@@ -6,7 +6,7 @@ import openpyxl
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-from weapon.models import GreatSword, LongSword, OneHandSword
+from weapon.models import *
 
 WB = openpyxl.load_workbook('weapon_data.xlsx')
 
@@ -180,7 +180,7 @@ def two_swords():
         else:
             slot = ws.cell(row=row, column=11).value
 
-        OneHandSword.objects.create(
+        TwoSwords.objects.create(
             name=name,
             rare=rare,
             produce=produce,
@@ -196,9 +196,56 @@ def two_swords():
     print('Two Swords Done')
 
 
+def hammer():
+
+    ws = WB['Hammer']
+
+    for row in range(1, 85):
+        name = ws.cell(row=row, column=1).value
+        rare = ws.cell(row=row, column=2).value
+        produce = ws.cell(row=row, column=3).value
+        attack = ws.cell(row=row, column=4).value
+        defense = ws.cell(row=row, column=5).value
+        if ws.cell(row=row, column=6).value is None:
+            weapon_property = '없음'
+            weapon_property_fiqure = '없음'
+        else:
+            weapon_property = ws.cell(row=row, column=6).value
+            weapon_property_fiqure = ws.cell(row=row, column=7).value
+
+        if ws.cell(row=row, column=8).value is None:
+            abnormal_condition = '없음'
+            abnormal_condition_figure = '없음'
+        else:
+            abnormal_condition = ws.cell(row=row, column=8).value
+            abnormal_condition_figure = ws.cell(row=row, column=9).value
+
+        fatal_blow = ws.cell(row=row, column=10).value
+        if ws.cell(row=row, column=11).value is None:
+            slot = '없음'
+        else:
+            slot = ws.cell(row=row, column=11).value
+
+        Hammer.objects.create(
+            name=name,
+            rare=rare,
+            produce=produce,
+            attack=attack,
+            defense=defense,
+            weapon_property=weapon_property,
+            weapon_property_fiqure=weapon_property_fiqure,
+            abnormal_condition=abnormal_condition,
+            abnormal_condition_figure=abnormal_condition_figure,
+            fatal_blow=fatal_blow,
+            slot=slot,
+        )
+    print('Hammer Done')
+
+
 if __name__ == "__main__":
 
     great_sword()
     long_sword()
     one_hand_sword()
     two_swords()
+    hammer()
--- app/weapon/models/__init__.py
+++ app/weapon/models/__init__.py
@@ -2,3 +2,4 @@ from .great_sword import *
 from .long_sword import *
 from .one_hand_sword import *
 from .two_swords import *
+from .hammer import *
