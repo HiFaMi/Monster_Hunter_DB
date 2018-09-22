362bf1eef40899b88230398376435ecbeabea855
mingyu <rlaalsrbgk@gmail.com>
2018-09-22 23:24:42 +0900

    armor model 추가

---
+++ app/armor/models/armor.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+__all__ = (
+    'Armor',
+)
+
+
+class Armor(models.Model):
+    name = models.CharField(max_length=100)
+    equip_region = models.CharField(max_length=100)
+    rare = models.CharField(max_length=50)
+    cost = models.IntegerField()
+    defense = models.IntegerField()
+    vs_fire = models.IntegerField()
+    vs_water = models.IntegerField()
+    vs_thunder = models.IntegerField()
+    vs_ice = models.IntegerField()
+    vs_dragon = models.IntegerField()
+    slot = models.CharField(max_length=50)
+    first_skill = models.CharField(max_length=150)
+    second_skill = models.CharField(max_length=150)
