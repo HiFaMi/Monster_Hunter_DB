deded392085635e8c9a2d11f398edaec35389d8f
mingyu <rlaalsrbgk@gmail.com>
2018-09-28 20:51:48 +0900

    skill many_to_many fields 작성

--- app/skill/models/skill.py
+++ app/skill/models/skill.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from armor.models.armor import Armor
+from items.models.accessory import Accessory
+
 __all__ = (
     'Skill',
 )
@@ -8,4 +11,11 @@ __all__ = (
 class Skill(models.Model):
     name = models.CharField(max_length=150)
     detail = models.CharField(max_length=300)
-
+    accessory = models.ManyToManyField(
+        Accessory,
+        related_name='accessory_skills',
+    )
+    armor = models.ManyToManyField(
+        Armor,
+        related_name='armor_skill',
+    )
