60a6a10f48caa34618a85a753fae8b6cf1867a3b
mingyu <rlaalsrbgk@gmail.com>
2018-09-29 17:49:55 +0900

    items model에 weapon 및armor 추가

--- app/items/models/item.py
+++ app/items/models/item.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from armor.models.armor import Armor
+from weapon.models import ChargeBlade, GreatSword, GunLance, Hammer, HeavyBowgun, Lance, LightBowgun, LongSword, \
+    OneHandSword, SwitchAxe, TwoSwords
+
 __all__ = (
     'Items',
 )
@@ -7,3 +11,73 @@ __all__ = (
 
 class Items(models.Model):
     name = models.CharField(max_length=200)
+    armor = models.ManyToManyField(
+        Armor,
+        related_name='makes_armor',
+    )
+
+    charge_blade = models.ManyToManyField(
+        ChargeBlade,
+        related_name='makes_charge_blade',
+        null=True,
+    )
+
+    great_sword = models.ManyToManyField(
+        GreatSword,
+        related_name='makes_great_sword',
+        null=True,
+    )
+
+    gun_lance = models.ManyToManyField(
+        GunLance,
+        related_name='makes_gun_lance',
+        null=True,
+    )
+
+    hammer = models.ManyToManyField(
+        Hammer,
+        related_name='makes_hammer',
+        null=True,
+    )
+
+    heavy_bowgun = models.ManyToManyField(
+        HeavyBowgun,
+        related_name='makes_heavy_bowgun',
+        null=True,
+    )
+
+    lance = models.ManyToManyField(
+        Lance,
+        related_name='makes_lance',
+        null=True,
+    )
+
+    light_bowgun = models.ManyToManyField(
+        LightBowgun,
+        related_name='makes_light_bowgun',
+        null=True,
+    )
+
+    long_sword = models.ManyToManyField(
+        LongSword,
+        related_name='makes_long_sword',
+        null=True,
+    )
+
+    one_hand_sword = models.ManyToManyField(
+        OneHandSword,
+        related_name='makes_heavy_bowgun',
+        null=True,
+    )
+
+    switch_axe = models.ManyToManyField(
+        SwitchAxe,
+        related_name='makes_heavy_bowgun',
+        null=True,
+    )
+
+    two_sowrds = models.ManyToManyField(
+        TwoSwords,
+        related_name='makes_heavy_bowgun',
+        null=True,
+    )
