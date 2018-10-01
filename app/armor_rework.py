a43dd2ddbf92a086a3d306c04fcc2f118c4690a0
mingyu <rlaalsrbgk@gmail.com>
2018-10-01 18:35:19 +0900

    skill이 자동적으로 split되게끔 구현

--- app/armor_rework.py
+++ app/armor_rework.py
@@ -1,22 +1,21 @@
 import openpyxl
 
 wb = openpyxl.load_workbook('armor_list.xlsx')
-ws = wb['Armor']
+ws = wb['Skill']
 
 
-for i in range(1, 755):
-    base = ws.cell(row=i, column=6).value
-    if base[2] is not '-':
-        ws.cell(row=i, column=7).value = base[2]
+for i in range(1, 144):
+    base = ws.cell(row=i, column=2).value
+    try:
+        skill_list = base.split('|')
+
+    except AttributeError:
+        ws.cell(row=i, column=3).value = base
+
     else:
-        ws.cell(row=i, column=7).value = base[2:4]
-
-    if base[2] is not '-':
-        ws.cell(row=i, column=7).value = base[2]
-    else:
-        ws.cell(row=i, column=7).value = base[2:4]
+        for index in range(len(skill_list)):
+            ws.cell(row=i, column=3+index).value = skill_list[index]
 
 wb.save('armor_list.xlsx')
 print('Rework Done')
-
 
