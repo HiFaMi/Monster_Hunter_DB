db8e908ac5e5615aef62294e6834f3ffe4d081fe
mingyu <rlaalsrbgk@gmail.com>
2018-10-02 22:14:32 +0900

    armor의 속성이 필요없는내용 삭제하게끔 전처리

--- app/armor_rework.py
+++ app/armor_rework.py
@@ -4,11 +4,17 @@ wb = openpyxl.load_workbook('armor_list.xlsx')
 ws = wb['Armor']
 
 
+def replace_multiple(main_string, to_be_replaces, new_string):
+    for elem in to_be_replaces:
+        if elem in main_string:
+            main_string = main_string.replace(elem, new_string)
+    return main_string
+
+
 for i in range(1, 144):
     base = ws.cell(row=i, column=6).value
-    skill_list = base.split('|')
-    for index in range(len(skill_list)):
-        ws.cell(row=i, column=7+index).value = skill_list[index]
+    ws.cell(row=i, column=7).value = replace_multiple(base, ['화', '수', '뇌', '빙', '용'], '')
+
 
 wb.save('armor_list.xlsx')
 print('Rework Done')
