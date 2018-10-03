ef0231aff7531c25b88b3d3d162d6c3383b97915
mingyu <rlaalsrbgk@gmail.com>
2018-10-03 22:14:30 +0900

    armor의 속성에 따른 값 자동으로 정리되게끔 구현

--- app/armor_rework.py
+++ app/armor_rework.py
@@ -13,7 +13,10 @@ def replace_multiple(main_string, to_be_replaces, new_string):
 
 for i in range(1, 144):
     base = ws.cell(row=i, column=6).value
-    ws.cell(row=i, column=7).value = replace_multiple(base, ['화', '수', '뇌', '빙', '용'], '')
+    replace_base = replace_multiple(base, ['화', '수', '뇌', '빙', '용'], '')
+    armor_list = replace_base.split(':')
+    for list_index in range(1, len(armor_list)):
+        ws.cell(row=i, column=6+list_index).value = armor_list[list_index]
 
 
 wb.save('armor_list.xlsx')
