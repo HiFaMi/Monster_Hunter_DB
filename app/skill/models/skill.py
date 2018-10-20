6523d53114524cdb9a381764175de50257b39e2d
mingyu <rlaalsrbgk@gmail.com>
2018-10-20 19:42:00 +0900

    skill model 변경 및 migrate

--- app/skill/models/skill.py
+++ app/skill/models/skill.py
@@ -10,7 +10,12 @@ __all__ = (
 
 class Skill(models.Model):
     name = models.CharField(max_length=150)
-    detail = models.CharField(max_length=300)
+    Lv1 = models.CharField(max_length=300, null=True)
+    Lv2 = models.CharField(max_length=300, null=True)
+    Lv3 = models.CharField(max_length=300, null=True)
+    Lv4 = models.CharField(max_length=300, null=True)
+    Lv5 = models.CharField(max_length=300, null=True)
+
     accessory = models.ManyToManyField(
         Accessory,
         related_name='accessory_skills',
