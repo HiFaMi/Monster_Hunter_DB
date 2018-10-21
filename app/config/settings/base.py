84fc39c81d0efa1d0b67992d1f0c1c91054beaec
mingyu <rlaalsrbgk@gmail.com>
2018-10-21 21:00:24 +0900

    base.html templates로 이동 및 secrets key 재설정

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -11,18 +11,22 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+SECRETS_DIR = os.path.join(ROOT_DIR, '.secrets')
 
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'secrets.json')))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&@_@d=z$w65_$i_zv&44gmlv$x#b=wk=d&b9(+(b2q0+9xi*ci'
+SECRET_KEY = secrets['SECRET_KEY']
 
 
 # Application definition
