86c5bf4de38cffe3994ef0a1a6c9ba5f3738f910
mingyu <rlaalsrbgk@gmail.com>
2018-11-01 22:38:13 +0900

    user custom 수정

--- app/members/models.py
+++ app/members/models.py
@@ -1,8 +1,21 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 from django.db import models
 
 # Create your models here.
 
 
+class UserManager(DjangoUserManager):
+    def create_django_user(self, *args, **kwargs):
+        user = User.objects.create_user(
+            username=kwargs.get('username'),
+            email=kwargs.get('email'),
+            password=kwargs.get('password'),
+            first_name=kwargs.get('first_name', ''),
+            phone_number=kwargs.get('phone_number', ''),
+        )
+        return user
+
+
 class User(AbstractUser):
-    user_id = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    create_date = models.DateField(auto_now_add=True)
