c35784ed88e2ad21074aa57bc753ac4ecfc74a6a
mingyu <rlaalsrbgk@gmail.com>
2018-11-05 21:42:25 +0900

    members view 설정

--- app/members/views/__init__.py
+++ app/members/views/__init__.py
@@ -1,3 +1,2 @@
-from django.shortcuts import render
-
-# Create your views here.
+from .user_list import *
+from .kakao_auth import *
---
+++ app/members/views/kakao_auth.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+from django.conf import settings
+from django.contrib.auth import get_user_model, authenticate
+from django.shortcuts import render
+
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from members.serializers.user import UserSerializer
+
+__all__ = (
+    'KakaoAuth',
+)
+
+User = get_user_model()
+
+
+class KakaoAuth(APIView):
+
+    def get(self, request):
+        code = request.GET.get('code')
+        user = authenticate(request, code=code)
+
+        token, __ = Token.objects.get_or_create(user=user)
+        data = {
+            'token': token.key,
+            'user': UserSerializer(user).data,
+        }
+
+        return Response(data)
