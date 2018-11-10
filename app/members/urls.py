9a5a873511eebb3469fd2d9e137a120512e87c15
mingyu <rlaalsrbgk@gmail.com>
2018-11-10 22:42:11 +0900

    kakao url 수정

--- app/members/urls.py
+++ app/members/urls.py
@@ -5,6 +5,6 @@ from .views import *
 app_name = 'user'
 
 urlpatterns = [
-    path('oauth/', kakao_auth.KakaoAuth.as_view(), name='kakao_auth'),
+    path('kakao_oauth/', kakao_auth.KakaoAuth.as_view(), name='kakao_auth'),
     path('naver_oauth/', naver_auth.NaverAuth.as_view(), name='naver_auth'),
 ]
