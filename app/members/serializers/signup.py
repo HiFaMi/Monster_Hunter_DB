6063e8dbf197f212298f93bbe4779bfb7bd43a42
mingyu <rlaalsrbgk@gmail.com>
2018-11-18 22:16:01 +0900

    send email을 위한 serializer 설정

---
+++ app/members/serializers/signup.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import get_user_model
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from rest_framework import serializers
+from ..tokens import account_activation_token
+
+User = get_user_model()
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'email_check',
+            'password',
+        )
+
+    def validated_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("비밀번호는 8글자 이상이여야 합니다.")
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=self.validated_data['username'],
+            password=self.validated_data['password']
+        )
+        # user.save()
+        # send_mail.delay(user.pk)
+
+        message = render_to_string('account_activate_email.html', {
+            'user': user,
+            'domain': 'devkmg.net',
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8'),
+            'token': account_activation_token.make_token(user)
+        })
+
+        mail_subject = 'test'
+        to_email = user.username
+        email = EmailMessage(mail_subject, message, to=[to_email])
+        email.send()
+
+        return validated_data
