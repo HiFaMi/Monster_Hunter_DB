62292dc3433ad9707d177e7a2c628b173aead87e
mingyu <rlaalsrbgk@gmail.com>
2018-11-18 22:22:18 +0900

    signup url 설정

--- app/members/urls.py
+++ app/members/urls.py
@@ -7,4 +7,5 @@ app_name = 'user'
 urlpatterns = [
     path('kakao_oauth/', kakao_auth.KakaoAuth.as_view(), name='kakao_auth'),
     path('naver_oauth/', naver_auth.NaverAuth.as_view(), name='naver_auth'),
+    path('signup/', signup.UserSignup.as_view(), name='signup'),
 ]
