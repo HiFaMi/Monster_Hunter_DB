e048a118e634a406d80c94f08cedbd7dc263ce9d
mingyu <rlaalsrbgk@gmail.com>
2018-11-26 21:57:43 +0900

    all auth url 작성

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import *
 
@@ -7,5 +7,8 @@ app_name = 'user'
 urlpatterns = [
     path('kakao_oauth/', kakao_auth.KakaoAuth.as_view(), name='kakao_auth'),
     path('naver_oauth/', naver_auth.NaverAuth.as_view(), name='naver_auth'),
+    path('local_oauth/', auth.UserAuth.as_view(), name='local_auth'),
     path('signup/', signup.UserSignup.as_view(), name='signup'),
+    path('accounts/login/', accounts_login.account_login, name='accounts_login'),
+    path('accounts/', include('allauth.urls')),
 ]
