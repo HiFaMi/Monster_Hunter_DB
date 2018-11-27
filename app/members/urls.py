8057bb8cd47cda2ad39d399d8b02c7b47f42fe92
mingyu <rlaalsrbgk@gmail.com>
2018-11-27 19:48:00 +0900

    kakao auth 및 github auth allauth 사용을 위해 url 변경

--- app/members/urls.py
+++ app/members/urls.py
@@ -5,10 +5,11 @@ from .views import *
 app_name = 'user'
 
 urlpatterns = [
+
     path('kakao_oauth/', kakao_auth.KakaoAuth.as_view(), name='kakao_auth'),
     path('naver_oauth/', naver_auth.NaverAuth.as_view(), name='naver_auth'),
     path('local_oauth/', auth.UserAuth.as_view(), name='local_auth'),
     path('signup/', signup.UserSignup.as_view(), name='signup'),
-    path('accounts/login/', accounts_login.account_login, name='accounts_login'),
-    path('accounts/', include('allauth.urls')),
+
+
 ]
